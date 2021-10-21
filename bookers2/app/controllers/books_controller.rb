2e35484a29e91e8853d42cdfdeee323a7693943f
T6776 <GitHubt930324@gmail.com>
2021-10-21 15:05:22 +0000

    [Update] 10/21

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -4,25 +4,40 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to user_path(@user.id)
+    redirect_to book_path(@book)
   end
 
   def index
-    @user = User.find(params[:id])
     @books = Book.all
+    @book = Book.new
+    @user = @book.user
   end
 
   def show
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
   private
 
-  def post_image_params
+  def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
 
