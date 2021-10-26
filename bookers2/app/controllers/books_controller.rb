499c836ccdc8e717272edd9ad86f4742e1431f8b
T6776 <t930324@gmail.com>
2021-10-26 17:54:26 +0000

    [Update]

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @post_comment = PostComment.new
   end
   
   def edit
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  
-  
+
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
 
-  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
